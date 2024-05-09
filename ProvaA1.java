d3ceed9976bdccf27e3d73910a4e1d0fe1345881
Arthur Nascimento <128861658+aqthurn@users.noreply.github.com>
2024-05-08 21:45:36 -0300

    Add files via upload

---
+++ ProvaA1.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner Ler = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = Ler.nextLine();
+        System.out.println("Digite o valor da sua compra: ");
+        double compra = Ler.nextDouble();
+        System.out.println("Digite o valor do pagamento: ");
+
+        double pagamento = Ler.nextDouble();
+
+        double[] notas = { 200.00, 100.00, 50.00, 20.00, 5.00, 2.00, 1.00 };
+        int[] quantidade = new int[notas.length];
+
+        double troco = compra - pagamento;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Valor Total da Compra R$: " + compra);
+        System.out.println("Valor Pago R$: " + pagamento);
+        System.out.println("Troco: " + troco);
+
+        int listaNotas = 0;
+        while (listaNotas < notas.length) {
+            quantidade[listaNotas] = (int) (troco / notas[listaNotas]);
+            troco %= notas[listaNotas];
+
+            System.out.println("Notas de " + notas[listaNotas] + ": " + quantidade[listaNotas]);
+            listaNotas++;
+        }
+    }
+}
